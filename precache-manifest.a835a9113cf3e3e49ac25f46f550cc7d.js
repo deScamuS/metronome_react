22291841256b63e7f41f8216cc838d4794bb81d1
Michael DeSantis <desanmichael1@gmail.com>
2019-11-23 09:35:23 -0600

    Updates

---
+++ precache-manifest.a835a9113cf3e3e49ac25f46f550cc7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318144bcbf44920bd0341b0a0e26dc60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8cba9e3b5cb0285a14f",
+    "url": "/static/css/main.c3e4c810.chunk.css"
+  },
+  {
+    "revision": "91407a10c994b05e53e4",
+    "url": "/static/js/2.81554063.chunk.js"
+  },
+  {
+    "revision": "f8cba9e3b5cb0285a14f",
+    "url": "/static/js/main.a0db9f03.chunk.js"
+  },
+  {
+    "revision": "ecdc1f494f4ad54b48d8",
+    "url": "/static/js/runtime-main.49da80fe.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/static/media/click2.fe0ded65.wav"
+  }
+]);
